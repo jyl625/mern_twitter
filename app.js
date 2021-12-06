39d0530d7011ab263b8a68c4e676523a51f94358
Jean Youn Lee <jean.youn.lee@gmail.com>
2021-12-05 23:14:19 -0800

    Implement working version

--- app.js
+++ app.js
@@ -1,28 +1,36 @@
-const mongoose = require('mongoose');
-
 const express = require("express");
 const app = express();
-
+const mongoose = require('mongoose');
 const db = require('./config/keys').mongoURI;
-
 const users = require("./routes/api/users");
 const tweets = require("./routes/api/tweets");
-
+const User = require('./models/User')
 const bodyParser = require('body-parser');
+
+const passport = require('passport')
+
+app.use(passport.initialize());
+
+require ('./config/passport')(passport)
 
 
 mongoose
   .connect(db, { useNewUrlParser: true })
   .then(() => console.log("Connected to MongoDB successfully"))
   .catch(err => console.log(err));
+  
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
-app.get("/", (req, res) => res.send("Hello World - Edited again"));
+app.get("/", (req, res) => {
+  res.send("Hello World - Edited again")
+});
+
+
 
 app.use("/api/users", users);
 app.use("/api/tweets", tweets);
 
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
 
 const port = process.env.PORT || 5000;
 
